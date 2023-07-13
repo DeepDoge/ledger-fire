c7b3d672571cb063074a74bea043ecf8a751f8c9
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-07-13 16:06:58 +0000

    ...

--- src/server/start.ts
+++ src/server/start.ts
@@ -1,13 +1,31 @@
-import Database from "better-sqlite3"
-import { drizzle } from "drizzle-orm/better-sqlite3"
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
+import Database from "npm:better-sqlite3"
+import type { InferModel } from "npm:drizzle-orm"
+import { drizzle } from "npm:drizzle-orm/better-sqlite3"
+import { blob, integer, sqliteTable, text } from "npm:drizzle-orm/sqlite-core"
 
-const users = sqliteTable("users", {
-	id: integer("id").primaryKey(), // 'id' is the column name
-	fullName: text("full_name"),
-})
-
-const sqlite = new Database("sqlite.db")
+const sqlite = new Database("generated/sqlite.db")
 const db = drizzle(sqlite)
 
-const allUsers = db.select().from(users).all()
+const transactions = sqliteTable("users", {
+	id: blob("id", { mode: "bigint" }).primaryKey(),
+	method: text("method").notNull(),
+	data: blob("data", { mode: "buffer" }).notNull(),
+	from: blob("from", { mode: "buffer" }).notNull(),
+	timestamp: integer("timestamp").notNull(),
+})
+
+export type Transaction = InferModel<typeof transactions>
+export namespace Transaction {
+	export function create(method: string, data: Uint8Array, signature: Uint8Array) {
+		return db
+			.insert(transactions)
+			.values({
+				id: BigInt(Date.now()),
+				method,
+				data,
+				from: new Uint8Array(),
+				timestamp: Date.now(),
+			})
+			.returning()
+	}
+}
