22d0f5c120955a49f27529afa198139c23319e4a
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-08-11 11:48:24 +0000

    ...

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -14,8 +14,8 @@ export namespace SearchManager {
 	// Hacks to get correct type from prisma, because prisma cant just generete good types
 	// @ts-ignore
 	function ItemTypeHelper<TQuery extends Query["findMany"], TArgs extends Prisma.Args>() {
-		const query = null as unknown as TQuery
-		return null as unknown as Awaited<ReturnType<typeof query<TArgs>>>[number]
+		const findMany = null as unknown as TQuery
+		return null as unknown as Awaited<ReturnType<typeof findMany<TArgs>>>[number]
 	}
 	export type Item<TQuery extends Query, TInclude extends Include<TQuery>> = Awaited<ReturnType<TQuery["findMany"]>>[number] &
 		ReturnType<typeof ItemTypeHelper<TQuery["findMany"], { include: TInclude }>>
